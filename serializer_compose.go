4fd197751fd1ab58ab932e589e80c4891c05a732
artarts36 <temicska99@mail.ru>
2026-04-14 00:08:46 +0300

    chore: remove ComposeWithJSON

--- serializer_compose.go
+++ serializer_compose.go
@@ -12,27 +12,6 @@ func NewComposeSerializer(byType map[SerializationType]Serializer) *ComposeSeria
 	return &ComposeSerializer{byType: byType}
 }
 
-// ComposeWithJSON returns a [ComposeSerializer] that always registers [NewJSONSerializer] for [SerializationJSON],
-// unless you override it by including [SerializationJSON] in extra.
-// Nil serializers in extra are skipped.
-func ComposeWithJSON(extra map[SerializationType]Serializer) *ComposeSerializer {
-	m := make(map[SerializationType]Serializer, len(extra)+1)
-
-	for k, v := range extra {
-		if v == nil {
-			continue
-		}
-
-		m[k] = v
-	}
-
-	if _, ok := m[SerializationJSON]; !ok {
-		m[SerializationJSON] = NewJSONSerializer()
-	}
-
-	return NewComposeSerializer(m)
-}
-
 func (c *ComposeSerializer) Serialize(event Event) ([]byte, error) {
 	meta := event.TopicMeta()
 	s, ok := c.byType[meta.SerializationType]
